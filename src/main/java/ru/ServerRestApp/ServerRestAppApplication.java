fe3f18af3f86e3ade5ef2ccdf152bd7bb4963416
UsikovKonstantin <99312736+UsikovKonstantin@users.noreply.github.com>
2023-06-19 10:25:31 +0300

    Инициализация проекта

---
+++ src/main/java/ru/ServerRestApp/ServerRestAppApplication.java
@@ -0,0 +1,13 @@
+package ru.ServerRestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerRestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ServerRestApp/ServerRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ServerRestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
