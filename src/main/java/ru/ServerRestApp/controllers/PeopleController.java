d4f6047354d0333257938d0a4b5ca7a06770e17d
UsikovKonstantin <99312736+UsikovKonstantin@users.noreply.github.com>
2023-06-22 15:50:30 +0300

    Обработка исключений для PeopleController

--- src/main/java/ru/ServerRestApp/controllers/PeopleController.java
+++ src/main/java/ru/ServerRestApp/controllers/PeopleController.java
@@ -4,11 +4,13 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import ru.ServerRestApp.models.Person;
+import ru.ServerRestApp.models.Team;
 import ru.ServerRestApp.services.PeopleService;
+import ru.ServerRestApp.services.TeamsService;
 import ru.ServerRestApp.util.ErrorResponse;
 import ru.ServerRestApp.util.DataException;
 import ru.ServerRestApp.util.NotFoundException;
@@ -23,60 +25,74 @@ import static ru.ServerRestApp.util.ErrorsUtil.returnDataErrorsToClient;
 public class PeopleController {
 
     private final PeopleService peopleService;
+    private final TeamsService teamsService;
+    private final PasswordEncoder passwordEncoder;
     @Autowired
-    public PeopleController(PeopleService peopleService) {
+    public PeopleController(PeopleService peopleService, TeamsService teamsService, PasswordEncoder passwordEncoder) {
         this.peopleService = peopleService;
+        this.teamsService = teamsService;
+        this.passwordEncoder = passwordEncoder;
     }
 
 
     @GetMapping()
-    public List<Person> getAllPeople() {
-        return peopleService.findAll();
+    public ResponseEntity<List<Person>> getAllPeople() {
+        List<Person> people = peopleService.findAll();
+        return new ResponseEntity<>(people, HttpStatus.OK);
     }
 
     @GetMapping("/teamId/{id}")
-    public List<Person> getPeopleByTeamId(@PathVariable("id") int id) {
-        return peopleService.findByTeamId(id);
+    public ResponseEntity<List<Person>> getPeopleByTeamId(@PathVariable("id") int id) {
+        Optional<Team> team = teamsService.findById(id);
+        if (team.isEmpty())
+            throw new NotFoundException("Team with this id wasn't found!");
+
+        List<Person> people = peopleService.findByTeamId(id);
+        return new ResponseEntity<>(people, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Person getPerson(@PathVariable("id") int id) {
+    public ResponseEntity<Person> getPerson(@PathVariable("id") int id) {
         Optional<Person> person = peopleService.findById(id);
-        if (person.isPresent())
-            return person.get();
-        else
+        if (person.isEmpty())
             throw new NotFoundException("Person with this id wasn't found!");
+        return new ResponseEntity<>(person.get(), HttpStatus.OK);
     }
 
     @GetMapping("/email/{email}")
-    public Person getPersonByEmail(@PathVariable("email") String email) {
+    public ResponseEntity<Person> getPersonByEmail(@PathVariable("email") String email) {
         Optional<Person> person = peopleService.findByEmail(email);
-        if (person.isPresent())
-            return person.get();
-        else
+        if (person.isEmpty())
             throw new NotFoundException("Person with this email wasn't found!");
+        return new ResponseEntity<>(person.get(), HttpStatus.OK);
     }
 
-    @PostMapping("/add")
-    public ResponseEntity<Person> addPerson(@RequestBody @Valid Person person, BindingResult bindingResult) {
+    @PostMapping("/update/{id}")
+    public ResponseEntity<Person> updatePerson(@PathVariable("id") int id, @RequestBody @Valid Person person, BindingResult bindingResult) {
 
+        person.setId(id);
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
         if (bindingResult.hasErrors())
             returnDataErrorsToClient(bindingResult);
 
-        peopleService.save(person);
+        Optional<Person> foundPerson = peopleService.findById(id);
+        if (foundPerson.isEmpty())
+            throw new NotFoundException("Person with this id wasn't found!");
+
+        if (person.getTeam() == null)
+            throw new DataException("Team must not be null!");
+
+        Optional<Team> team = teamsService.findById(person.getTeam().getId());
+        if (team.isEmpty())
+            throw new NotFoundException("Team with this id wasn't found!");
+
+        Optional<Person> found_person = peopleService.findByEmail(person.getEmail());
+        if (found_person.isPresent() && found_person.get().getId() != person.getId())
+            throw new DataException("Person with this email already exists");
+
+        peopleService.update(person);
 
         return new ResponseEntity<>(person, HttpStatus.OK);
-    }
-
-    @PostMapping("/update")
-    public Person updatePerson(@RequestBody Person person) {
-        peopleService.update(person);
-        return peopleService.findById(person.getId()).get();
-    }
-
-    @PostMapping("/delete/{id}")
-    public void deleteTeam(@PathVariable("id") int id) {
-        peopleService.delete(id);
     }
 
 
