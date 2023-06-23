3df918f6c80081ceafd628f73c306fa7a3d0fbbb
Дмитрий Орлов <dimanchik2003@mail.ru>
2023-06-23 22:11:11 +0300

    Бред

--- src/main/java/ru/ServerRestApp/ServerRestAppApplication.java
+++ src/main/java/ru/ServerRestApp/ServerRestAppApplication.java
@@ -5,9 +5,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerRestAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ServerRestAppApplication.class, args);
-	}
-
+	public static void main(String[] args) {SpringApplication.run(ServerRestAppApplication.class, args);}
 }
