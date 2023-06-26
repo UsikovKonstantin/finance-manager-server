2b09925d05c0216637e652e82739dc51dafe4d86
UsikovKonstantin <99312736+UsikovKonstantin@users.noreply.github.com>
2023-06-26 20:13:10 +0300

    fix

--- src/main/java/ru/ServerRestApp/JWT/config/SecurityConfiguration.java
+++ src/main/java/ru/ServerRestApp/JWT/config/SecurityConfiguration.java
@@ -28,6 +28,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
